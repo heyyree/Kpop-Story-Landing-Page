0f37921c92a15da3268702b3ef90f36309203f4c
Hearhee Kang <114882595+heyyree@users.noreply.github.com>
2022-10-20 17:38:55 -0400

    Update and rename apps.js to script.js

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar_menu');
-
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
-});
-
 const productContainers = [...document.querySelectorAll('.product-container')];
 const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
 const preBtn = [...document.querySelectorAll('.pre-btn')];
-const nxtBtn2 = [...document.querySelectorAll('.nxt-btn2')];
-const preBtn2 = [...document.querySelectorAll('.pre-btn2')];
+
 
 productContainers.forEach((item, i) => {
     let containerDimensions = item.getBoundingClientRect();
